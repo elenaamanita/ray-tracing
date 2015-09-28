6efc0f8ec3524ad629af8978244b7b06d6eae6ea
Eleni <elenipaskali@yahoo.com>
2015-09-28 21:21:28 +0100

    ray tracing
    Ray trace a living room

---
+++ Assets/script/RayTracerObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayTracerObject : MonoBehaviour {
+
+    public float lambertCoeff = 1f;
+
+    public float reflectiveCoeff = 0f;
+
+    public float phongCoeff = 1f;
+    public float phongPower = 2f;
+
+    public float blinnPhongCoeff = 1f;
+    public float blinnPhongPower = 2f;
+
+    public float transparentCoeff = 0f;
+
+
+    public Color baseColor = Color.gray;
+
+    void Awake()
+    {
+        if (!renderer.material.mainTexture)
+        {
+            renderer.material.color = baseColor;
+        }
+    }
+}
